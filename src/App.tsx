6fba2a0aca836801edfd15b25271f570d98a43b4
TheLuckyman30 <bnauta0@gmail.com>
2025-03-15 16:15:05 -0400

    Added backend server

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { useEffect, useState } from "react";
 import './App.css'
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    // Make a GET request to the backend
+    fetch("http://localhost:3001")
+      .then((response) => response.text())
+      .then((data) => setMessage(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
   return (
     <div>
-      Hello World
-    </div> 
-  )
+      {message}
+    </div>
+  );
 }
 
-export default App
+export default App;
