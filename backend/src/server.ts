320e36c904b0174072085497f69865f225d7adb8
TheLuckyman30 <bnauta0@gmail.com>
2025-03-15 18:14:45 -0400

    Updated backend to handle google client account

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,35 @@
 import express, { Request, Response } from "express";
+import { google } from 'googleapis'
+import { JWT } from "google-auth-library";
 import cors from "cors";
+import dotenv from 'dotenv';
 
 const backend = express();
 const PORT = 3001;
+dotenv.config();
 
 backend.use(cors());
 
-backend.get("/", (req: Request, res: Response) => {
-  res.send("Sus");
+const SCOPES = ['https://www.googleapis.com/auth/documents.readonly'];
+const KEY_PATH = process.env.PATH;
+
+
+backend.get("/", async (req: Request, res: Response) => {
+
+  const auth = new JWT({
+    keyFile: KEY_PATH,
+    scopes: SCOPES
+  });
+
+  const document = google.docs({ 
+    version: 'v1', auth: auth 
+  });
+
+  const myDoc = await document.documents.get({
+    documentId: '1hV9vn0l1r1j0b7Y1yRd2HjtMLaGhwBuh1BHHTTsL2J8'
+  });
+
+  res.send(myDoc.data.body);
 });
 
 backend.listen(PORT, () => {
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { docs_v1 } from 'googleapis'
 import './App.css'
 
 function App() {
+  const [text, setText] = useState<string[]>([]);
 
-  useEffect(() => {
+  // useEffect(() => {
     
-    fetch('http://localhost:3001')
-      .then((response) => response.text())
-      .then((data) => console.log(data));
-  }, []);
+  //   fetch('http://localhost:3001')
+  //     .then((response) => response.json())
+  //     .then((document: docs_v1.Schema$Body) => {
+  //       const contents = document?.content
+  //       if (contents) {
+  //         const temp: string[] = [];
+  //         contents.forEach((content) => {
+  //           const elements = content.paragraph?.elements;
+  //           if (elements) {
+  //             elements.forEach((element) => {
+  //               if (element.textRun?.content) {
+  //                 temp.push(element.textRun.content.toString())
+  //                 setText(temp);
+  //               }
+  //             });
+  //           }
+  //         });
+  //       }
+  //     });
+  // }, []);
 
   return (
     <div>
-      Temp
+      {text}
     </div>
   );
 }
