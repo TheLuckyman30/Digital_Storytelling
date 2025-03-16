224c967af113ba858620bf07ce7152931edfa2dd
TheLuckyman30 <bnauta0@gmail.com>
2025-03-16 12:01:41 -0400

    Added parallax scrolling and styled text boxes

--- src/App.tsx
+++ src/App.tsx
@@ -7,34 +7,47 @@ function App() {
   const [text, setText] = useState<string[]>([]);
 
   function getContent () {
+    const temp: string[] = [];
+    let body: string = '';
     fetch('http://localhost:3001')
       .then((response) => response.json())
       .then((document: docs_v1.Schema$Body) => {
         const contents = document?.content
         if (contents) {
-          const temp: string[] = [];
           contents.forEach((content) => {
             const elements = content.paragraph?.elements;
             if (elements) {
               elements.forEach((element) => {
                 if (element.textRun?.content) {
-                  temp.push(element.textRun.content.toString())
-                  setText(temp);
+                  const stringifiedContent = element.textRun.content;
+                  if (stringifiedContent === '\n') {
+                    temp.push(body);
+                    setText(temp);
+                    body = '';
+                  }
+                  else {
+                    body += stringifiedContent + "  \n";
+                  }
                 }
               });
             }
           });
         }
+      })
+      .then(() => {
+        if (body.length) {
+          temp.push(body);
+          setText(temp);
+        }
       });
-    
   }
 
   return (
     <div>
       <div onClick={getContent} className="button">Get Content</div>
       <div>
-        {text.map((t: string) => 
-          <TextComponent text={t}></TextComponent>
+        {text.map((t: string, index: number) => 
+          <TextComponent text={t} key={index}></TextComponent>
         )}
       </div>
     </div>
--- src/components/TextComponent.tsx
+++ src/components/TextComponent.tsx
@@ -6,7 +6,7 @@ interface TextComponentProps {
 
 function TextComponent ({text}: TextComponentProps) {
     return (
-        <div style={{margin: '5rem', border: '1px solid black'}}>
+        <div style={{margin: '5rem', padding: '1rem', borderRadius: '15px', backgroundColor: 'white', boxShadow: '1rem 1rem 1rem rgb(1 0 0 / 30%)'}}>
             <ReactMarkdown>{text}</ReactMarkdown>
         </div>
         
