0632a4d118882e70e85fdc8b1d2dd60b9ecd23fc
TheLuckyman30 <bnauta0@gmail.com>
2025-03-17 16:24:55 -0400

    Updated styling

--- src/components/TextComponent.tsx
+++ src/components/TextComponent.tsx
@@ -6,7 +6,7 @@ interface TextComponentProps {
 
 function TextComponent ({text}: TextComponentProps) {
     return (
-        <div style={{ padding: '1rem', margin: '5rem', borderRadius: '15px', backgroundColor: 'white', boxShadow: '1rem 1rem 1rem rgb(1 0 0 / 30%)', width: '50rem'}}>
+        <div style={{ padding: '1rem', margin: '10rem', borderRadius: '15px', backgroundColor: 'white', boxShadow: '1rem 1rem 1rem rgb(1 0 0 / 30%)', width: '40rem'}}>
             <ReactMarkdown>{text}</ReactMarkdown>
         </div>
         
