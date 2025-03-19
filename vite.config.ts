8beb7b377f02881798dbc055045e6a3d1721ef31
TheLuckyman30 <bnauta0@gmail.com>
2025-03-19 04:05:14 -0400

    Removed code from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    cors: {
-      // the origin you will be accessing via browser
-      origin: "https://158.101.106.176:3001/",
-    },
-  },
-  build: {
-    // generate .vite/manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: '/path/to/main.js',
-    },
-  },
   plugins: [react()], base: '/Digital_Storytelling/'
 })
