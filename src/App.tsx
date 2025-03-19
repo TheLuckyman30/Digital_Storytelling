51a91fe7c28c4760c1ac5531bf2e7ed61fb6a3e5
TheLuckyman30 <bnauta0@gmail.com>
2025-03-19 02:11:06 -0400

    Moved url

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
+import { JSX, useState } from "react";
 import { docs_v1 } from 'googleapis'
 import './css/App.css'
 import TextComponent from "./components/TextComponent";
+import img1 from './assets/Background.png'
 
 function App() {
-  const [text, setText] = useState<string[]>([]);
-
+  const [text, setText] = useState<JSX.Element[]>([]);
+  document.body.style.backgroundImage=`url(${img1})`
+  
   function getContent () {
-    const temp: string[] = [];
+    const temp: JSX.Element[] = [];
     let body: string = '';
-    fetch('http://localhost:3001')
+    fetch(import.meta.env.VITE_FETCH_URL)
       .then((response) => response.json())
       .then((document: docs_v1.Schema$Body) => {
         const contents = document?.content
@@ -21,7 +23,8 @@ function App() {
                 if (element.textRun?.content) {
                   const stringifiedContent = element.textRun.content;
                   if (stringifiedContent === '\n') {
-                    temp.push(body);
+                    const textCompnent = <TextComponent text={body}></TextComponent>
+                    temp.push(textCompnent);
                     setText(temp);
                     body = '';
                   }
@@ -36,7 +39,8 @@ function App() {
       })
       .then(() => {
         if (body.length) {
-          temp.push(body);
+          const textCompnent = <TextComponent text={body}></TextComponent>
+          temp.push(textCompnent);
           setText(temp);
         }
       });
@@ -44,11 +48,9 @@ function App() {
 
   return (
     <div className="main">
-      {!text.length && <div onClick={getContent} className="button">Get Content</div>}
+      {!text.length && <div onClick={getContent} className="button">Get Contnet</div>}
       <div>
-        {text.map((t: string, index: number) => 
-          <TextComponent text={t} key={index}></TextComponent>
-        )}
+        {text.map((text) => text)}
       </div>
     </div>
   );
